a000c0a06a1a99fc17dfa8bb1ca38f7736be218f
Claire Ramming <ceramming@gmail.com>
2018-02-28 16:21:28 -0500

    my solutions to chapter 2 & 3 exercises

--- code/modefunctions_mysolution.py
+++ code/modefunctions_mysolution.py
@@ -1,27 +1,18 @@
 """My solution to exercise 2.3"""
-import thinkplot
+import thinkstats2
 
 def Mode(hist):
     """returns mode. Does not return freq value related to the mode.
     Use AllModes with num=1 for that functionality."""
-    mode = 0
-    mode_val = 'empty'
-    for val, freq in hist.Items():
-        if mode < freq:
-            mode = freq
-            mode_val = val
-    return mode_val
+    freq, val = max([(freq, val) for val, freq in hist.Items()])
+    return val
 
 def AllModes(hist, num=0):
     """returns all modes, also added support for
     returning specific number of modes.
     Setting num=1 gives same as Mode function but also
     returns frequency value."""
-    modes = []
-    print('Val | Freq')
-    for val, freq in hist.Items():
-        modes.append((freq, val))
-    sort_modes = sorted(modes, reverse=True)
+    sort_modes = sorted(hist.Items(), key=lambda row: row[1], reverse=True)
     if not num:
         num = len(sort_modes)
     for freq, val in sort_modes[0:num]:
